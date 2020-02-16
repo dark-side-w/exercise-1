4327c32902ae74085a3262050e9ede61b6cc0f97
Dariusz Wytrykus <d.wytrykus@gmail.com>
2020-02-16 23:21:14 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+(() => {
+    console.log('Tak, to w co jest opkowane to wszytko to IIFE - Immediately invoked function expression');
+
+    // Dane z backednu
+    const user = {
+        name: 'Księciunio',
+        email: 'k@example.com',
+        permissions: [
+            'edit',
+            'copy',
+            'delete'
+        ],
+        isAdmin: true
+    }
+    // TODO: Zadania dla Ciebie:
+    // 1. Dla każdej właściwości stwórz zmieną która będzie przechowywwać jej wartość
+    // 2. Stwórz warunke który wyświetli w konsoli adres email jeżeli użytkownik posiada uprawnienie do kopiowania.
+    // Zadanie dodatkowe:
+    // 3. Zmień nazwę buttona 'Sign up' na nazwę użytkownika.
+
+
+})();
